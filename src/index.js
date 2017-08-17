aa6d1db7c06f29674332829293741bf56b6d817e
Laurel Herting <laurel.herting@gmail.com>
2017-08-17 15:52:40 -0600

    Add constructor to refactor components - state

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  // TODO: remove the constructor
   constructor() {
     super();
     this.state = {
@@ -11,6 +12,8 @@ class Square extends React.Component {
   }
 
   render() {
+    // TODO: use onClick={this.props.onClick}
+    // TODO: replace this.state.value with this.props.value
     return (
       <button className="square" onClick={() => this.setState({value: 'X'})}>
         {this.state.value}
@@ -20,10 +23,17 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
   }
 
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]} />;
+  }
+  
   render() {
     const status = 'Next player: X';
 
@@ -31,19 +41,13 @@ class Board extends React.Component {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
         </div>
       </div>
     );
