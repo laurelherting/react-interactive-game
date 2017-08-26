e6aa04820a11b7346476c99455f519e6b621f8dc
Laurel Herting <laurel.herting@gmail.com>
2017-08-25 18:55:45 -0600

    add key

--- src/index.js
+++ src/index.js
@@ -80,7 +80,7 @@ class Game extends React.Component {
         'Move #' + move :
         'Game start';
       return (
-        <li>
+        <li key={move}>
           <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
         </li>
       );
